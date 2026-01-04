4a06c6179dc1249d368b50352409f9d536b92f3e
Marcio Mallqui <marolwyn@gmail.com>
2026-01-04 18:10:13 -0500

    Actualización de la base de datos Tienda Aurelion

--- Creación de la base de datos/base de datos/Creación de tablas y agregación de datos/Creación de la base de datos.sql
+++ Creación de la base de datos/base de datos/Creación de tablas y agregación de datos/Creación de la base de datos.sql
@@ -3,12 +3,12 @@ CREATE DATABASE Proyecto_Tienda_Aurelion;
 
 USE Proyecto_Tienda_Aurelion;
 
-<<<<<<< HEAD
--- Tabla Categorías
-=======
+
+-- Tabla Categorías --
+----------------------
 -- Categorías
 
->>>>>>> marcio_mallqui_analyst
+
 CREATE TABLE categorias (
 	id_categoria INT PRIMARY KEY IDENTITY(1,1),
 	categoria VARCHAR(50)
@@ -18,12 +18,11 @@ INSERT INTO dbo.categorias(categoria)
 VALUES ('Alimentos'),
 	   ('Limpieza');
 
-
-
 -- SELECT * FROM dbo.categorias;
 
 
--- Tabla Productos
+-- Tabla Productos --
+---------------------
 
 CREATE TABLE productos (
 	id_producto INT PRIMARY KEY IDENTITY(1,1),
@@ -134,30 +133,13 @@ VALUES (	'Coca Cola 1.5L'	,	2347	,	1	),
 			(	'Esponjas x3'	,	2430	,	1	),
 			(	'Trapo de Piso'	,	4854	,	2	);
 
+
 -- SELECT * FROM dbo.productos;
 
 
 
--- Tabla Productos
-
-CREATE TABLE productos (
-	id_producto INT PRIMARY KEY IDENTITY(1,1),
-	nombre_producto TEXT,
-	precio_unitario FLOAT(1),
-	id_categoria INT FOREIGN KEY REFERENCES categorias(id_categoria)
-);
-
-INSERT INTO productos(nombre_producto, precio_unitario, id_categoria)
-VALUES ('sabana', 25.5, 3);
-
-INSERT INTO productos(nombre_producto, precio_unitario, id_categoria)
-VALUES ('sabana', 50.78, 2);
-
-SELECT * FROM dbo.productos
-
-
-
--- Tabla Medio de Pago
+-- Tabla Medio de Pago --
+-------------------------
 
 CREATE TABLE medio_de_pago (
 	id_medio_pago INT PRIMARY KEY IDENTITY(1,1),
@@ -173,7 +155,8 @@ VALUES (	'tarjeta'	),
 -- SELECT * FROM medio_de_pago;
 
 
--- Tabla Ciudades
+-- Tabla Ciudades --
+--------------------
 
 CREATE TABLE ciudades (
 	id_ciudad INT PRIMARY KEY IDENTITY(1,1),
@@ -192,7 +175,8 @@ VALUES  (	'Carlos Paz'	),
 -- SELECT * FROM ciudades;
 
 
--- Tabla Clientes
+-- Tabla Clientes --
+--------------------
 
 CREATE TABLE clientes (
 	id_cliente INT PRIMARY KEY IDENTITY(1,1),
@@ -308,7 +292,8 @@ VALUES (	'Mariana Lopez'	,	'mariana.lopez@mail.com'	,	'2023-01-01'	,	1	),
 -- SELECT * FROM dbo.clientes;
 
 
--- tabla Ventas
+-- tabla Ventas --
+------------------
 
 CREATE TABLE ventas (
 	id_venta INT PRIMARY KEY IDENTITY(1,1),
@@ -440,10 +425,11 @@ VALUES  (	'2024-06-19'	,	62	,	1	),
 		(	'2024-04-21'	,	72	,	1	);
 
 
-SELECT * FROM dbo.ventas;
+-- SELECT * FROM dbo.ventas;
 
 
--- Tabla Detalle Ventas
+-- Tabla Detalle Ventas --
+--------------------------
 
 CREATE TABLE detalle_ventas (
 	id_venta INT FOREIGN KEY REFERENCES ventas(id_venta),
@@ -803,76 +789,20 @@ VALUES 	(	1	,	90	,	1	,	2902	),
 
 
 
+-- Eliminar tablas. --
+----------------------
 
+-- DROP TABLE dbo.detalle_ventas;
 
+-- DROP TABLE dbo.productos; --
 
+-- DROP TABLE dbo.categorias; --
 
--- Eliminar tablas.
+-- DROP TABLE dbo.medio_de_pago; --
 
-DROP TABLE dbo.detalle_ventas;
+-- DROP TABLE dbo.ventas;
 
-DROP TABLE dbo.productos; --
+-- DROP TABLE dbo.clientes; --
 
-DROP TABLE dbo.categorias; --
-
-DROP TABLE dbo.medio_de_pago; --
-
-DROP TABLE dbo.ventas;
-
-DROP TABLE dbo.clientes; --
-
-DROP TABLE dbo.ciudades; --
-
-
-
-
--- Tabla Clientes
-
-CREATE TABLE clientes (
-	id_cliente INT PRIMARY KEY IDENTITY(1,1),
-	nombre_cliente TEXT,
-	email TEXT,
-	fecha_alta DATE,
-	id_ciudad INT FOREIGN KEY REFERENCES ciudades(id_ciudad)
-);
-
-INSERT INTO dbo.clientes (nombre_cliente, email, fecha_alta, id_ciudad)
-VALUES ('Juan Alberto', 'mar_olwyn@gmail.com', '2026-01-03', 2);
-
-INSERT INTO dbo.clientes (nombre_cliente, email, fecha_alta, id_ciudad)
-VALUES ('Juan davalos', 'davalos@gmail.com', '2026-01-02', 1);
-
-
-SELECT * FROM dbo.clientes;
-
-
-
--- tabla Ventas
-
-CREATE TABLE ventas (
-	id_venta INT PRIMARY KEY IDENTITY(1,1),
-	fecha DATE,
-	id_cliente INT FOREIGN KEY REFERENCES clientes(id_cliente),
-	id_medio_pago INT FOREIGN KEY REFERENCES medio_de_pago(id_medio_pago)
-);
-
-INSERT INTO dbo.ventas (fecha, id_cliente, id_medio_pago)
-VALUES ('2026-01-04', 2, 1);
-
-SELECT * FROM dbo.ventas;
-
--- Tabla Detalle Ventas
-
-CREATE TABLE detalle_ventas (
-	id_venta INT FOREIGN KEY REFERENCES ventas(id_venta),
-	id_producto INT FOREIGN KEY REFERENCES productos(id_producto),
-	cantidad INT,
-	precio_unitario FLOAT(1),
-	importe FLOAT(1)
-);
-
-INSERT INTO detalle_ventas (id_venta, id_producto, cantidad, precio_unitario, importe)
-VALUES (1,1,3,5.6, 16.8);
-
-SELECT * FROM detalle_ventas;
+-- DROP TABLE dbo.ciudades; --
 
